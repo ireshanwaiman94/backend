cef160f82ba0183f33e1ec3c6841740a8130519b
ireshanwaiman94 <98511645+ireshanwaiman94@users.noreply.github.com>
2023-03-16 21:58:18 +0530

    index.js and package.json files have been commited

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static('build'))
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
